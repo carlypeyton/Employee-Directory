436994238952d3369256810873b2323d69b22b2d
Carly Peyton <carlypeyton@gmail.com>
2021-04-19 13:20:08 -0400

    Create Employee Table.js.

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = "https://randomuser.me/api/?results=300&nat=us";
+const URL = "https://randomuser.me/api/?results=100&nat=us";
 
 export default {
   getEmployees: function () {
