7df971e9da76f497bd5b54edbae9cb2215410757
Carly Peyton <carlypeyton@gmail.com>
2021-04-19 11:45:17 -0400

    Update App and index.js.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from "react";
-import "./style/style.css";
-import Header from "./components/Header";
-import MainContainer from "./components/MainContainer";
+import MainContainer from "./components/MainContainer"
 
 function App() {
   return (
-    <div>
-      <Header />
-      <MainContainer />
-    </div>
-  );
-};
+    <MainContainer />
+  )
+}
 
 export default App;
