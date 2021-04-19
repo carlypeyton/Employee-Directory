c64de97978c2814377b77f61f6416d7ce94e7703
Carly Peyton <carlypeyton@gmail.com>
2021-04-19 15:10:43 -0400

    Add more search results to ensure correct filtering.

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = "https://randomuser.me/api/?results=100&nat=us";
+const URL = "https://randomuser.me/api/?results=500&nat=us";
 
 export default {
   getEmployees: function () {
