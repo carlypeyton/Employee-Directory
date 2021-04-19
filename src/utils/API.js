5f438150b5d8d07e6acc61f3c79d69613ca9d2b0
Carly Peyton <carlypeyton@gmail.com>
2021-04-19 11:49:08 -0400

    Update formatting of API.js.

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const URL = "https://randomuser.me/api/?results=300&nat=us";
 
 export default {
-    getEmployees: function () {
-        return axios.get(URL);
-    }
+  getEmployees: function () {
+    return axios.get(URL);
+  }
 };
